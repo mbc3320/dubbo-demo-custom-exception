e6bbf99bb2cbb0e2e1391473580376799514de50
BeanShell.More <mobinchao@hotmail.com>
2020-03-03 12:09:51 +0800

    add get set

--- dubbo-demo1-common/src/main/java/cn/c3p0/cloud/demo1/exception/BaseException.java
+++ dubbo-demo1-common/src/main/java/cn/c3p0/cloud/demo1/exception/BaseException.java
@@ -1,7 +1,6 @@
 package cn.c3p0.cloud.demo1.exception;
 
 import cn.c3p0.cloud.demo1.enums.ValueEnum;
-import org.apache.dubbo.rpc.RpcException;
 
 public class BaseException extends RuntimeException {
 
@@ -10,6 +9,14 @@ public class BaseException extends RuntimeException {
     public BaseException(ValueEnum status) {
 //        super(status.getCode(), status.getText());
         super(status.getText());
+        this.status = status;
+    }
+
+    public ValueEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(ValueEnum status) {
         this.status = status;
     }
 }
--- dubbo-demo1-common/src/main/java/cn/c3p0/cloud/demo1/exception/BaseRpcException.java
+++ dubbo-demo1-common/src/main/java/cn/c3p0/cloud/demo1/exception/BaseRpcException.java
@@ -1,7 +1,7 @@
 package cn.c3p0.cloud.demo1.exception;
 
 import cn.c3p0.cloud.demo1.enums.ValueEnum;
-import org.apache.dubbo.rpc.RpcException;
+import com.alibaba.dubbo.rpc.RpcException;
 
 public class BaseRpcException extends RpcException {
 
@@ -9,6 +9,14 @@ public class BaseRpcException extends RpcException {
 
     public BaseRpcException(ValueEnum status) {
         super(status.getCode(), status.getText());
+        this.status = status;
+    }
+
+    public ValueEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(ValueEnum status) {
         this.status = status;
     }
 }
