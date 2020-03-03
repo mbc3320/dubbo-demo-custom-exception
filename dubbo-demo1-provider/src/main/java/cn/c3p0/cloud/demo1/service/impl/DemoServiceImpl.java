bd93f9425cb1e59649577317bb2bae9d3b4393df
BeanShell.More <mobinchao@hotmail.com>
2020-03-03 09:36:36 +0800

    增加调用日志

--- dubbo-demo1-provider/src/main/java/cn/c3p0/cloud/demo1/service/impl/DemoServiceImpl.java
+++ dubbo-demo1-provider/src/main/java/cn/c3p0/cloud/demo1/service/impl/DemoServiceImpl.java
@@ -4,18 +4,22 @@ import cn.c3p0.cloud.demo1.exception.DemoException;
 import cn.c3p0.cloud.demo1.exception.DemoRpcException;
 import cn.c3p0.cloud.demo1.exception.status.DemoStatusCode;
 import cn.c3p0.cloud.demo1.service.DemoService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Service;
 
+@Slf4j
 @Service
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public Boolean testException() {
+        log.info("try base exception...");
         throw new DemoException(DemoStatusCode.ERROR1);
     }
 
     @Override
     public Boolean testRpcException() {
+        log.info("try rpc exception...");
         throw new DemoRpcException(DemoStatusCode.ERROR1);
     }
 }
